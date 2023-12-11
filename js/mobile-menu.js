b13052f93fe522ee0db387f86025caf1bde9eaa8
Rostik Novosad <rostiknovosad4002@gmail.com>
2023-12-11 21:15:24 +0200

    update mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,11 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuBtnOpen = document.querySelector(".menu-btn-open");
 const menuBtnClose = document.querySelector(".menu-btn-close");
+const mobileMenuSkills = document.querySelector("#mobile-menu-skills");
+const mobileMenuPortfolio = document.querySelector("#mobile-menu-portfolio");
+const mobileMenuVideo = document.querySelector("#mobile-menu-video");
+const mobileMenuPrice = document.querySelector("#mobile-menu-price");
+const mobileMenuContact = document.querySelector("#mobile-menu-contact");
 
 const toggleMenu = () => mobileMenu.classList.toggle("is-open");
 const disableScroll = () =>
@@ -8,6 +13,16 @@ const disableScroll = () =>
 
 menuBtnOpen.addEventListener("click", toggleMenu);
 menuBtnClose.addEventListener("click", toggleMenu);
+mobileMenuSkills.addEventListener("click", toggleMenu);
+mobileMenuPortfolio.addEventListener("click", toggleMenu);
+mobileMenuVideo.addEventListener("click", toggleMenu);
+mobileMenuPrice.addEventListener("click", toggleMenu);
+mobileMenuContact.addEventListener("click", toggleMenu);
 
 menuBtnOpen.addEventListener("click", disableScroll);
 menuBtnClose.addEventListener("click", disableScroll);
+mobileMenuSkills.addEventListener("click", disableScroll);
+mobileMenuPortfolio.addEventListener("click", disableScroll);
+mobileMenuVideo.addEventListener("click", disableScroll);
+mobileMenuPrice.addEventListener("click", disableScroll);
+mobileMenuContact.addEventListener("click", disableScroll);
